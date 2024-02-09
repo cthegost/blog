80f6270b1456104eed582cb67c98babffe420bef
cthegost <57126417+cthegost@users.noreply.github.com>
2024-02-10 02:11:35 +0300

    first changes, added header, logo, styles and more

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  // useTabs: false,
+  // semi: true,
+  // tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'none',
+  // bracketSpacing: true,
+  // arrowParens: "avoid"
+};
